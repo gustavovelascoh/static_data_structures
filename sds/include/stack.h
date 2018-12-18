5c2061ddf756be0a58db7f2fff6be91f546d352f
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2018-12-18 23:30:21 +0000

    Updated stack files

--- sds/include/stack.h
+++ sds/include/stack.h
@@ -16,9 +16,10 @@ typedef struct {
     void * array;
     uint16_t size;
     uint16_t index;
+    uint8_t element_size;
 } stack_t;
 
-int16_t stack_init(stack_t * stack, void * array, uint16_t size);
+int16_t stack_init(stack_t * stack, void * array, uint16_t size, uint8_t element_size);
 int16_t stack_push(stack_t * stack, void * data);
 int16_t stack_pop(stack_t * stack, void * data);
 
--- sds/src/stack.c
+++ sds/src/stack.c
@@ -1,8 +1,10 @@
 #include "stack.h"
+#include "string.h"
 
-int16_t stack_init(stack_t * stack, void * array, uint16_t size){
+int16_t stack_init(stack_t * stack, void * array, uint16_t size, uint8_t element_size){
     stack->array = array;
     stack->size = size;
+    stack->element_size = element_size;
     stack->index = 0;
 
     return 0;
@@ -11,7 +13,7 @@ int16_t stack_init(stack_t * stack, void * array, uint16_t size){
 int16_t stack_push(stack_t * stack, void * data){
     int16_t ret = -1;
     if (stack->index < stack->size){
-        *(stack->array)[stack->index] = *data;
+        memcpy(stack->array + (stack->index * stack->element_size), data, stack->element_size);
         stack->index++;
         ret = 0;
     }
@@ -21,8 +23,9 @@ int16_t stack_push(stack_t * stack, void * data){
 int16_t stack_pop(stack_t * stack, void * data){
     int16_t ret = -1;
     if (stack->index > 0){
-        *data = *(stack->array)[stack->index];
-        return 0;
+        stack->index--;
+        memcpy(data, stack->array + (stack->index * stack->element_size), stack->element_size);
+        ret = 0;
     }
     return ret;
 }
