a19939a383bd6e9244a5438665b1a69cd4c26da6
Gustavo Velasco-Hernandez <gustavo.velascoh@gmail.com>
2019-02-10 22:29:18 +0000

    Updated files

--- examples/05-permissions/permissions.c
+++ examples/05-permissions/permissions.c
@@ -1,14 +1,25 @@
 #include "permissions.h"
+#include "ll.h"
 
+stack_t permissions_ll_s;
+permission_t permissions_ll_a[PERM_LL_SIZE];
+ll_node_t * permissions_ll_sa[PERM_LL_SIZE];
+ll_node_t permissions_ll_na[PERM_LL_SIZE];
+ll_t permissions_ll;
+char permissions_md5[32];
 
 void permissions_init(){
-    
+    ll_init(&permissions_ll, permissions_ll_na, &permissions_ll_a,
+        &permissions_ll_s, permissions_ll_sa, PERM_LL_SIZE,
+        sizeof(permission_t));
 }
 
-void permissions_add(permissions_t p);
+void permissions_add(permission_t p);
 
 void permissions_remove(uint32_t id);
 
 void permissions_flush();
 
 uint8_t permissions_validate(tag_t rfid);
+
+void permissions_calc_md5();
--- examples/05-permissions/permissions.h
+++ examples/05-permissions/permissions.h
@@ -1,5 +1,5 @@
 /*
- * permssions.h
+ * permissions.h
  *
  *  Created on: Feb 10, 2019
  *      Author: gustavo
@@ -8,18 +8,24 @@
  */
 #include <stdint.h>
 
+#define PERM_LL_SIZE 10
+
 typedef char tag_t[10];
 
  typedef struct {
      tag_t rfid;
      uint32_t id;
      uint8_t mask;
- } permissions_t;
+ } permission_t;
 
 void permissions_init();
 
-void permissions_add(permissions_t p);
+void permissions_add(permission_t p);
 
 void permissions_remove(uint32_t id);
 
 void permissions_flush();
+
+uint8_t permissions_validate(tag_t rfid);
+
+void permissions_calc_md5();
